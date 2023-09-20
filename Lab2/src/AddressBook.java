bcee2e098bc850c1d3c95ddfc3ad07b42982fa84
Tony <137113805+TonySitu@users.noreply.github.com>
2023-09-20 13:10:25 -0400

    initial commmit

--- Lab2/src/AddressBook.java
+++ Lab2/src/AddressBook.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
 public class AddressBook {
+    private final ArrayList<BuddyInfo> buddyList;
+
+    public AddressBook() {
+        buddyList = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        buddyList.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        String name = buddy.getName();
+        buddyList.removeIf(i -> i.getName().equals(name));
+    }
 }
